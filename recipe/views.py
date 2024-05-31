7d13d740a74cbcf5bcfc45f3ac36c18b4fb81e12
YuraK26MC <yura_kodenov@knu.ua>
2024-05-31 17:22:40 +0300

    add render

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
 def main(request):
     return render(request, 'recipe/main.html')
+def category_detail(request, category_slug):
+    return render((request, 'recipe/category_detail.html'))
